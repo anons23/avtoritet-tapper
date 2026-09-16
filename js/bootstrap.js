3720b5bbf4b307b0d2d8250fa8a9469528bd95af
anons23 <51492752+anons23@users.noreply.github.com>
2026-09-16 09:34:58 +0600

    chore: bump NPC dialogue cache version

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -8,7 +8,7 @@
     './js/name-ui.js?v=2.1',
     './js/prison-ui.js?v=2.0',
     './js/rank-ui.js?v=2.2',
-    './js/npc-ui.js?v=2.3',
+    './js/npc-ui.js?v=2.4',
     './js/health-ui.js?v=3.3',
     './js/stories-ui-v2.js?v=2.1'
   ];
