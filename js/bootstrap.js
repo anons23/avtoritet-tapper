275f66dd356cd2a3a4331ec458627a1ae4815af7
anons23 <51492752+anons23@users.noreply.github.com>
2026-09-16 09:45:58 +0600

    cache: bump mission and gym UI versions

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,12 +3,12 @@
   const scripts=[
     './js/input-guard.js?v=1.1',
     './js/save-migration.js?v=1.0',
-    './js/game.js?v=4.3',
-    './js/shop-ui.js?v=2.0',
+    './js/game.js?v=4.4',
+    './js/shop-ui.js?v=2.1',
     './js/name-ui.js?v=2.1',
     './js/prison-ui.js?v=2.0',
     './js/rank-ui.js?v=2.2',
-    './js/npc-ui.js?v=2.4',
+    './js/npc-ui.js?v=2.5',
     './js/health-ui.js?v=3.3',
     './js/stories-ui-v2.js?v=2.1'
   ];
