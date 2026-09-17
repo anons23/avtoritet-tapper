f1c43be1c848e360b59f86a99d3585e79c11e642
anons23 <51492752+anons23@users.noreply.github.com>
2026-09-17 10:31:33 +0600

    Disable legacy NPC stat rewards

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -8,6 +8,7 @@
     {src:'./js/name-ui.js?v=2.1',critical:false},
     {src:'./js/prison-ui.js?v=3.2',critical:false},
     {src:'./js/rank-ui.js?v=2.2',critical:false},
+    {src:'./js/npc-stat-cleanup.js?v=1.1',critical:false},
     {src:'./js/npc-ui.js?v=2.7',critical:false},
     {src:'./js/npc-no-emoji.js?v=1.0',critical:false},
     {src:'./js/npc5-ui.js?v=1.0',critical:false},
